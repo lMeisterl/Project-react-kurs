91c07c8a2ba91ad55b92710e38e087aefabdeb5e
Meister <shtokal2004@mail.ru>
2024-02-20 19:11:59 +0300

    очистка формы

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -5,29 +5,31 @@ import JournalList from './components/JournalList/JournalList';
 import LeftPanel from './layout/LeftPanel/LeftPanel';
 import Body from './layout/Body/Body';
 import JournalForm from './components/JournalForm/JournalForm';
-import { useState } from 'react';
-
-const INITIAL_DATA = [
-	// {
-	// 	id: 1,
-	// 	title: 'Подготовка к обновлению курса',
-	// 	text: 'пупупу',
-	// 	date: new Date()
-	// },
-	// {
-	// 	id: 2,
-	// 	title: 'Поход в горы',
-	// 	text: 'Думал что очень много времени',
-	// 	date: new Date()
-	// }
-];
+import { useEffect, useState } from 'react';
 
 function App() {
-	const [items, setItems] = useState(INITIAL_DATA);
+	const [items, setItems] = useState([]);
+
+	useEffect(() =>{
+		const data = JSON.parse(localStorage.getItem('data'));
+		if (data){
+			setItems(data.map(item => ({
+				...item,
+				date: new Date(item.date)
+			})));
+		}
+	}, []);
+
+	useEffect(() => {
+		if (items.length){
+			console.log('Запись!');
+			localStorage.setItem('data', JSON.stringify(items));
+		}
+	}, [items]);
 
 	const additem = item => {
 		setItems(oldItems => [...oldItems, {
-			text: item.text,
+			post: item.post,
 			title: item.title,
 			date: new Date(item.date),
 			id: oldItems.length > 0 ? Math.max(...oldItems.map(i => i.id))+1 : 1
--- react-app/src/components/JournalForm/JournalForm.jsx
+++ react-app/src/components/JournalForm/JournalForm.jsx
@@ -1,52 +1,77 @@
 import styles from './JournalForm.module.css';
 import Button from '../Button/Button';
-import { useState } from 'react';
+import { useEffect, useReducer} from 'react';
+import cn from 'classnames';
+import { INITIAL_STATE, formReducer } from './JournalForm.state';
 
 function JournalForm({ onSubmit }) {
+	const [formState, dispathForm] = useReducer(formReducer, INITIAL_STATE);
+	const { isValid, isFormReadyToSubmit, values } = formState;
+	
 
-	const [formValidState, setFromValidState] =useState({
-		title: true,
-		post: true,
-		date: true
-	});
+	useEffect(() => {
+		let timerId;
+		if (!isValid.date || !isValid.post || !isValid.title) {
+			timerId = setTimeout(() => {
+				console.log('Очистка формы');
+				dispathForm({type: 'RESET_VALIDITY'});
+			}, 2000);
+		}
+		return () => {
+			clearTimeout(timerId);
+		};
+	}, [isValid]);
+
+	useEffect (() => {
+		if (isFormReadyToSubmit){
+			onSubmit(values);
+			dispathForm({ type: 'CLEAR'});
+		}
+	}, [isFormReadyToSubmit]);
+
+	const onChange = (e) => {
+		dispathForm({ type: 'SET_VALUE', payload: {[e.target.name]:[e.target.value]}});
+	};
 
 	const addJournalItem = (e) => {
 		e.preventDefault();
 		const fromData =new FormData(e.target);
 		const formProps = Object.fromEntries(fromData);
-		let isFormValid = true;
-		if(!formProps.title?.trim().length){
-			setFromValidState(state => ({...state, title: false}));
-			isFormValid = false;
-		} else{
-			setFromValidState(state => ({...state, title: true}));
-		}
-		if(!formProps.post?.trim().length){
-			setFromValidState(state => ({...state, post: false}));
-			isFormValid = false;
-		} else{
-			setFromValidState(state => ({...state, post: true}));
-		}
-		if(!formProps.date){
-			setFromValidState(state => ({...state, date: false}));
-			isFormValid = false;
-		} else{
-			setFromValidState(state => ({...state, date: true}));
-		}
-		if(!isFormValid){
-			return;
-		}
-		onSubmit(formProps);
+		dispathForm({ type: 'SUBMIT', payload: formProps});
 	};
 	//Первый способ применить стиль className={`${styles['input']} ${formValidState.title ? '' : styles['invalid']}`}`
 	//Второй способ применить стиль style={{ border:  formValidState.date ? undefined : '1px solid red' }}
 	//Первый более облегчённый 
+	// Третий способ с использование библиотеки classNames 
+	//className={cn(styles['input'],{
+	// 	[styles['invalid']] : !formValidState.title
+	// })}
 	return(
 		<form className={styles['journal-form']} onSubmit={addJournalItem}>
-			<input type='title' name='title' className={`${styles['input']} ${formValidState.title ? '' : styles['invalid']}`}/>
-			<input type='date' name='date'className={`${styles['input']} ${formValidState.date ? '' : styles['invalid']}`}/>
-			<input type='text' name='tag'/>
-			<textarea name='post' id='' cols="30" rows="10" className={`${styles['input']} ${formValidState.post ? '' : styles['invalid']}`}></textarea>
+			<div>
+				<input type='title'onChange={onChange} value={values.title} name='title'  className={cn(styles['input-title'],{
+					[styles['invalid']] : !isValid.title
+				})}/>
+			</div>
+			<div className={styles['form-row']}>
+				<label htmlFor="date" className={styles['form-labels']}>
+					<img src="/calendar1.png" alt="Иконка календарь" />
+					<span>Дата</span>
+				</label>
+				<input type='date'onChange={onChange} name='date' id='date' value={values.date} className={cn(styles['input'],{
+					[styles['invalid']] : !isValid.date
+				})}/>
+			</div>
+			<div className={styles['form-row']}>
+				<label htmlFor="tag" className={styles['form-labels']}>
+					<img src="/folder1.png" alt="Иконка папки" />
+					<span>Метки</span>
+				</label>
+				<input type='text'onChange={onChange} name='tag' value={values.text} id='tag' className={styles['input']}/>
+			</div>
+			<textarea name='post'onChange={onChange} value={values.post} id='' cols="30" rows="10" className={cn(styles['input'],{
+				[styles['invalid']] : !isValid.post
+			})}></textarea>
 			<Button text="Сохранить"/>
 		</form>
 	);

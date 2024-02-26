7ce7d57fab3f6b0085a4662644d210725f4d044f
Meister <shtokal2004@mail.ru>
2024-02-26 19:47:07 +0300

    Focus

--- react-app/src/components/JournalForm/JournalForm.jsx
+++ react-app/src/components/JournalForm/JournalForm.jsx
@@ -1,19 +1,38 @@
 import styles from './JournalForm.module.css';
 import Button from '../Button/Button';
-import { useEffect, useReducer} from 'react';
+import { useEffect, useReducer, useRef} from 'react';
 import cn from 'classnames';
 import { INITIAL_STATE, formReducer } from './JournalForm.state';
+import Input from '../input/input';
 
 function JournalForm({ onSubmit }) {
 	const [formState, dispathForm] = useReducer(formReducer, INITIAL_STATE);
 	const { isValid, isFormReadyToSubmit, values } = formState;
+	//фокус на не заполненое поле
+	const titleRef = useRef();
+	const dateRef = useRef();
+	const postRef = useRef();
 	
+	const focusError = (isValid) => {
+		switch(true){
+		case !isValid.title:
+			titleRef.current.focus();
+			break;
+		case !isValid.date:
+			dateRef.current.focus();
+			break;
+		case !isValid.post:
+			postRef.current.focus();
+			break;
+
+		}
+	};
 
 	useEffect(() => {
 		let timerId;
 		if (!isValid.date || !isValid.post || !isValid.title) {
+			focusError(isValid);
 			timerId = setTimeout(() => {
-				console.log('Очистка формы');
 				dispathForm({type: 'RESET_VALIDITY'});
 			}, 2000);
 		}
@@ -27,10 +46,10 @@ function JournalForm({ onSubmit }) {
 			onSubmit(values);
 			dispathForm({ type: 'CLEAR'});
 		}
-	}, [isFormReadyToSubmit]);
+	}, [isFormReadyToSubmit, values, onSubmit]);
 
 	const onChange = (e) => {
-		dispathForm({ type: 'SET_VALUE', payload: {[e.target.name]:[e.target.value]}});
+		dispathForm({ type: 'SET_VALUE', payload: {[e.target.name]:e.target.value}});
 	};
 
 	const addJournalItem = (e) => {
@@ -49,27 +68,23 @@ function JournalForm({ onSubmit }) {
 	return(
 		<form className={styles['journal-form']} onSubmit={addJournalItem}>
 			<div>
-				<input type='title'onChange={onChange} value={values.title} name='title'  className={cn(styles['input-title'],{
-					[styles['invalid']] : !isValid.title
-				})}/>
+				<Input type='title' onChange={onChange} ref={titleRef} isValid = {isValid.title} value={values.title} name='title' appearence = "title" />
 			</div>
 			<div className={styles['form-row']}>
 				<label htmlFor="date" className={styles['form-labels']}>
 					<img src="/calendar1.png" alt="Иконка календарь" />
 					<span>Дата</span>
 				</label>
-				<input type='date'onChange={onChange} name='date' id='date' value={values.date} className={cn(styles['input'],{
-					[styles['invalid']] : !isValid.date
-				})}/>
+				<Input type='date' ref = {dateRef} onChange={onChange} isValid = {isValid.date} name='date' id='date' value={values.date} />
 			</div>
 			<div className={styles['form-row']}>
 				<label htmlFor="tag" className={styles['form-labels']}>
 					<img src="/folder1.png" alt="Иконка папки" />
 					<span>Метки</span>
 				</label>
-				<input type='text'onChange={onChange} name='tag' value={values.text} id='tag' className={styles['input']}/>
+				<Input type='text'onChange={onChange} name='tag' value={values.text} id='tag' />
 			</div>
-			<textarea name='post'onChange={onChange} value={values.post} id='' cols="30" rows="10" className={cn(styles['input'],{
+			<textarea name='post' ref={postRef} onChange={onChange} value={values.post} id='' cols="30" rows="10" className={cn(styles['input'],{
 				[styles['invalid']] : !isValid.post
 			})}></textarea>
 			<Button text="Сохранить"/>
